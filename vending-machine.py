ccd02431499aafa4a15263f21fae76c39e268caf
Ubuntu <ubuntu@ip-172-31-95-149.ec2.internal>
2019-06-27 17:43:15 +0000

    refactored code

--- vending-machine.py
+++ vending-machine.py
@@ -1,32 +1,30 @@
 from byotest import*
 
+usd_coins = [100,50,25,10,5,2,1]
+eur_coins = [100,50,20,10,5,2,1]
 
 #Get change function
-def get_change(amount):
-    if amount == 0:
-        return []
-    elif amount in [100,50,20,10,5,2,1]:
-        return [amount]
-    else:
+def get_change(amount,coin_list=eur_coins):
         change = []
-        
-        for coin in [100,50,20,10,5,2,1]:
-            if coin <= amount:
+        for coin in coin_list:
+            while coin <= amount:
                 amount -= coin
                 change.append(coin)
         return change   
     
         
 #Tests
-test_are_equal(get_change(0),[])
-test_are_equal(get_change(1),[1])
-test_are_equal(get_change(2),[2])
-test_are_equal(get_change(5),[5])
-test_are_equal(get_change(10),[10])
-test_are_equal(get_change(20),[20])
-test_are_equal(get_change(50),[50])
-test_are_equal(get_change(100),[100])
-test_are_equal(get_change(3),[2,1])
-test_are_equal(get_change(7),[5,2])
+test_are_equal(get_change(0,eur_coins),[])
+test_are_equal(get_change(1,eur_coins),[1])
+test_are_equal(get_change(2,eur_coins),[2])
+test_are_equal(get_change(5,eur_coins),[5])
+test_are_equal(get_change(10,eur_coins),[10])
+test_are_equal(get_change(20,eur_coins),[20])
+test_are_equal(get_change(50,eur_coins),[50])
+test_are_equal(get_change(100,eur_coins),[100])
+test_are_equal(get_change(3,eur_coins),[2,1])
+test_are_equal(get_change(7,eur_coins),[5,2])
+test_are_equal(get_change(9,eur_coins),[5,2,2])
+test_are_equal(get_change(75,usd_coins),[50,25])
 
 print("All tests passed!")
