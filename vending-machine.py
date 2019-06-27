f88a44b01605b50b416e58c6d6875f7098ea8654
Ubuntu <ubuntu@ip-172-31-95-149.ec2.internal>
2019-06-27 17:10:59 +0000

    basic code added to achieve functionality

--- vending-machine.py
+++ vending-machine.py
@@ -5,8 +5,17 @@ from byotest import*
 def get_change(amount):
     if amount == 0:
         return []
-    else:
+    elif amount in [100,50,20,10,5,2,1]:
         return [amount]
+    else:
+        change = []
+        
+        for coin in [100,50,20,10,5,2,1]:
+            if coin <= amount:
+                amount -= coin
+                change.append(coin)
+        return change   
+    
         
 #Tests
 test_are_equal(get_change(0),[])
@@ -17,5 +26,7 @@ test_are_equal(get_change(10),[10])
 test_are_equal(get_change(20),[20])
 test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
+test_are_equal(get_change(3),[2,1])
+test_are_equal(get_change(7),[5,2])
 
 print("All tests passed!")
